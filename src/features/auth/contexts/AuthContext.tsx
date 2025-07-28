46c5e630d8283584446b2792a706314153af58bd
haraldkangschroeder <haraldkangschroederdevelopment@gmail.com>
2025-07-28 16:36:09 +0200

    smd

---
+++ src/features/auth/contexts/AuthContext.tsx
@@ -0,0 +1,121 @@
+"use client";
+
+import {
+  createContext,
+  PropsWithChildren,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
+import { GraphService, MsalService } from "@bosch-gs-bda-apps/msal-wrapper";
+import getConfig from "next/config";
+import { ResponseType } from "@microsoft/microsoft-graph-client";
+// import { Loader } from "@/features/common/components/Loader";
+
+// const { publicRuntimeConfig } = getConfig();
+
+type AuthAppContext = {
+  currentUser?: BrainUser;
+  userPhoto?: string;
+  getAccessToken?: string;
+};
+
+export interface BrainUser {
+  displayName: string;
+  givenName: string;
+  userId: string;
+  userInitials: string;
+}
+
+export const AuthContext = createContext<AuthAppContext>({});
+
+const config = {
+  auth: {
+    clientId: process.env.AZURE_AD_CLIENT_ID as string,
+    authority: process.env.AZURE_AD_TENANT_ID,
+    knownAuthorities: [process.env.AZURE_AD_TENANT_ID],
+    redirectUri: "/",
+  },
+  cache: {
+    cacheLocation: "sessionStorage",
+  },
+};
+
+const scopes = {
+  scopes: {
+    backend: ["api://dia-brain/full-access"],
+    graph: ["profile", "User.ReadBasic.All", "Files.Read.All"],
+  },
+};
+
+export function AuthProvider({ children }: PropsWithChildren) {
+  const [currentUser, setCurrentUser] = useState<BrainUser>();
+  const [userPhoto, setUserPhoto] = useState<string | undefined>(undefined);
+  const [loading, setLoading] = useState(true);
+
+  async function fetchUser() {
+    console.log("test");
+    return await MsalService.instance.graph({
+      endpoint: "me",
+      responseType: ResponseType.JSON,
+    });
+  }
+
+  function createInitials(userDisplayName: string) {
+    return userDisplayName
+      .split(" ")
+      .slice(0, -1)
+      .map((el) => el[0])
+      .join("");
+  }
+
+  useEffect(() => {
+    (async () => {
+      setLoading(true);
+      try {
+        new MsalService(config as any, scopes);
+      } catch (e) {
+        // error => msal instance already defined
+      }
+      // await MsalService.instance.initialize();
+      await MsalService.instance.handleRedirectPromise();
+      const user = await fetchUser();
+      setCurrentUser({
+        displayName: user.displayName ?? "",
+        givenName: user.givenName || "",
+        userId: user.userPrincipalName!.split("@")[0],
+        userInitials: createInitials(user.displayName ?? ""),
+      });
+
+      try {
+        const picture = await GraphService(
+          MsalService.instance,
+        ).fetchCurrentUserProfilePicture();
+        setUserPhoto(picture);
+      } catch (e) {
+        new Error("User has no profile picture", { cause: e });
+      } finally {
+        setLoading(false);
+      }
+    })();
+  }, []);
+
+  const contextValue = useMemo(
+    () => ({
+      currentUser,
+      userPhoto,
+    }),
+    [currentUser, userPhoto],
+  );
+
+  if (loading) {
+    // return <Loader />;
+    return "TODO Loading...";
+  }
+
+  return (
+    <AuthContext.Provider value={contextValue}>
+      {currentUser && children}
+    </AuthContext.Provider>
+  );
+}
--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,17 +1,21 @@
 import Axios from "axios";
 import getConfig from "next/config";
+import { MsalService } from "@bosch-gs-bda-apps/msal-wrapper";
 
-const { publicRuntimeConfig } = getConfig();
+// const { publicRuntimeConfig } = getConfig();
 
-export const graphAxios = Axios.create({
-  baseURL: publicRuntimeConfig["API_URL"],
-});
+// export const graphAxios = Axios.create({
+//   baseURL: publicRuntimeConfig["API_URL"],
+// });
+
+export const graphAxios = Axios.create();
 
 graphAxios.interceptors.request.use(async (config) => {
-  // const token =
-  //   (await MsalService.instance.getGraphApiToken()) as any as string;
-
-  const token = ""; //TODO: Replace with actual token retrieval logic above as soon as MsalService is implemented
+  console.log("test");
+  const token =
+    (await MsalService.instance.getGraphApiToken()) as any as string;
+  console.log(token);
+  // const token = ""; //TODO: Replace with actual token retrieval logic above as soon as MsalService is implemented
 
   config.headers.authorization = `Bearer ${token}`;
   config.headers.Accept = "application/json";
