0fb072e1152f906b88adb114a5812637baa6760d
haraldkangschroeder <haraldkangschroederdevelopment@gmail.com>
2025-07-28 18:28:19 +0200

    feat(layout): make layout use client for now

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,19 @@
+"use client";
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/features/auth/contexts/AuthContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "dia-pomodoro - Focus Better, Achieve More",
-  description: "The ultimate Pomodoro timer for teams",
-};
+// export const metadata: Metadata = {
+//   title: "dia-pomodoro - Focus Better, Achieve More",
+//   description: "The ultimate Pomodoro timer for teams",
+// };
+
+const queryClient = new QueryClient();
 
 export default function RootLayout({
   children,
@@ -18,7 +23,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <AuthProvider>{children}</AuthProvider>
+        <QueryClientProvider client={queryClient}>
+          <AuthProvider>{children}</AuthProvider>
+        </QueryClientProvider>
       </body>
     </html>
   );
