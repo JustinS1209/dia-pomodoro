873fd6fa46217585b29378e683f883900adf40c4
haraldkangschroeder <haraldkangschroederdevelopment@gmail.com>
2025-07-30 09:32:02 +0200

    feat(calendar): convert to iso string when sending date time to fetch calendar events

--- src/features/calendar/api/index.ts
+++ src/features/calendar/api/index.ts
@@ -2,11 +2,14 @@ import axios from "axios";
 import { CalendarData } from "@/features/calendar/configurations/types";
 
 // TODO test for correctness
-export function fetchCalendarEvents(
+export async function fetchCalendarEvents(
   startDate: Date,
   endDate: Date,
   userPrincipalName: string,
 ): Promise<CalendarData[]> {
-  const requestUrl = `https://graph.microsoft.com/v1.0/users/${userPrincipalName}/calendarview?startdatetime=${startDate}&enddatetime=${endDate}`;
-  return (await axios.get(requestUrl)).value;
+  const start = startDate.toISOString();
+  const end = endDate.toISOString();
+  const requestUrl = `https://graph.microsoft.com/v1.0/users/${userPrincipalName}/calendarview?startdatetime=${start}&enddatetime=${end}`;
+  const response = await axios.get(requestUrl);
+  return response.data.value;
 }
