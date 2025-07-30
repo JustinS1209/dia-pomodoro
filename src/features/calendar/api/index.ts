108c619f8c733b43b9b86d8863993f3f4036597e
haraldkangschroeder <haraldkangschroederdevelopment@gmail.com>
2025-07-30 13:52:12 +0200

    feat(calendar-me)

--- src/features/calendar/api/index.ts
+++ src/features/calendar/api/index.ts
@@ -6,6 +6,18 @@ import {
 } from "@/features/calendar/configurations/types";
 import { graphAxios } from "@/lib/axios";
 
+export async function fetchCalendarEventsMe(
+  startDate: Date,
+  endDate: Date,
+  userPrincipalName: string,
+): Promise<CalendarData[]> {
+  const start = startDate.toISOString();
+  const end = endDate.toISOString();
+  const requestUrl = `https://graph.microsoft.com/v1.0/users/${userPrincipalName}/calendarview?startdatetime=${start}&enddatetime=${end}`;
+  const response = await graphAxios.get(requestUrl);
+  return response.data.value;
+}
+
 export async function fetchCalendarEvents(
   startDate: Date,
   endDate: Date,
--- src/features/calendar/utils/index.ts
+++ src/features/calendar/utils/index.ts
@@ -1,10 +1,20 @@
-import { fetchCalendarEvents } from "@/features/calendar/api";
+import { fetchCalendarEvents, fetchCalendarEventsMe } from "@/features/calendar/api";
 import { CalendarData } from "@/features/calendar/configurations/types";
 
 export function fetchCalendarEventsForToday(
   userPrincipalName: string,
 ): Promise<CalendarData[]> {
   return fetchCalendarEvents(
+    new Date(new Date().setHours(0, 0, 0, 0)),
+    new Date(new Date().setHours(23, 59, 59, 999)),
+    userPrincipalName,
+  );
+}
+
+export function fetchCalendarEventsMeForToday(
+  userPrincipalName: string,
+): Promise<CalendarData[]> {
+  return fetchCalendarEventsMe(
     new Date(new Date().setHours(0, 0, 0, 0)),
     new Date(new Date().setHours(23, 59, 59, 999)),
     userPrincipalName,
